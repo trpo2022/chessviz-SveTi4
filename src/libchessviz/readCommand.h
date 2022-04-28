872a42ed0dcefae2b2f9ce5a302d9dc95e8d6df1
SveTi4H <studymail2.7.53@gmail.com>
2022-04-28 17:57:03 +0700

    Deleted unnecessary files

--- src/libchessviz/readCommand.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include <libchessviz/swap.h>
-#include <libchessviz/step.h>
-using namespace std;
-
-void readCommand(string line, char board[N1][N2]);
