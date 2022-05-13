0dbf148e18244377730c6abc105f708779d01bdd
SveTi4H <studymail2.7.53@gmail.com>
2022-05-13 23:12:35 +0700

    Edit printboard function

--- src/libchessviz/print_board.cpp
+++ src/libchessviz/print_board.cpp
@@ -5,11 +5,12 @@ void PrintBoard(char (*board)[N])
     clearm();
     for (int i = 0; i < N; i++)
     {
+        cout << Column[i] << "|";
         for (int j = 0; j < N; j++)
         {
             cout << " " << board[i][j];
         }
         cout << endl;
     }
-    cout << endl;
+    cout << " +—————————————————\n   a b c d e f g h\n" << endl;
 }
