83219bc0e70a224e06ae4b3ba23cce2c80cfb227
SveTi4H <studymail2.7.53@gmail.com>
2022-05-14 10:17:56 +0700

    Introduced function declarations and created a new structure

--- src/libchessviz/chess.h
+++ src/libchessviz/chess.h
@@ -14,13 +14,24 @@ struct f {
     char name[1];
 };
 
+struct motion {
+    int yStart = 0;
+    int xStart = 0;
+    int yEnd = 0;
+    int xEnd = 0;
+    char move;
+};
+
 struct list_command {
     std::string Cmd;
     list_command *next, *prev;
 };
 void clearm();
+void Errors(int error);
 void PrintBoard(char (*board)[N]);
 void AddSpisElm(list_command*& tail, std::string line, int L, int R);
 void FillBoard(char (*board)[N]);
-void ReadSpis(list_command* head);
+void GetStep(motion* stp, char (*board)[N]);
+int Rules(std::string step, motion*& stp, char (*board)[N]);
+void ClearSpis(list_command* head);
 void check_txt(std::string file_name, list_command* tail);
