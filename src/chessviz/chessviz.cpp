322eb4b884f76d70e881819408ef8459308b92d3
SveTi4H <studymail2.7.53@gmail.com>
2022-05-14 10:18:35 +0700

    Edit main file

--- src/chessviz/chessviz.cpp
+++ src/chessviz/chessviz.cpp
@@ -6,14 +6,30 @@
 
 int main()
 {
-    
+    motion* stp;
+    stp = new motion;
     list_command *head, *tail;
     head = tail = new list_command;
     head->next = head->prev = NULL;
     char board[N][N] = {0};
     FillBoard(board);
+    clearm();
     PrintBoard(board);
     check_txt("board.txt", tail);
-    ReadSpis(head);
+
+    string cmd = {0};
+    list_command* p;
+    for (p = head->next; p; p = p->next) {
+        cmd = p->Cmd;
+        cout << p->Cmd << "\n" << endl;
+        if (Rules(cmd, stp, board) == 0)
+            break;
+        else if (Rules(cmd, stp, board) == 1) {
+            GetStep(stp, board);
+            PrintBoard(board);
+        }
+    }
+
+    ClearSpis(head);
     return 0;
 }
